5f4f57e32ef05c366e05222638c733803225e4bc
Tricky Patrick <decroix-thomas@wanadoo.fr>
2018-10-03 00:38:32 +0200

    Added bumpers (every 2 balls) and Level randomize

--- KnightBall/scripts/other_scripts/GameLogic.lua
+++ KnightBall/scripts/other_scripts/GameLogic.lua
@@ -16,9 +16,10 @@ local RandomBall
 local Balls1 = {}
 local Balls2 = {}
 local Balls3= {}
-
+local AddBumper = 0
 function lol ()
 RandomBall = GeneratedBall()
+AddBumper = AddBumper + 1
 	if NbGoalBlue<10 and NbGoalRed<10 and RandomBall==1 then 
 		createBall1()
 	end
@@ -37,6 +38,194 @@ function GeneratedBall(x)
 	c = math.random(0, 1)
 	math.floor(c)
 	n = 1+p+c
+	return n
+end
+
+function GeneratedBumper(x)
+	math.randomseed(os.clock()*100000000000)
+	p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+	
+	n = 1+p+c+pp+cc+ppp+ccc+pppp+cccc
+	return n
+end
+
+function GeneratedBumperLargeur(x)
+	math.randomseed(os.clock()*100000000000)
+	p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+		p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+		p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+	
+	n = 1+p+c+pp+cc+ppp+ccc+pppp+cccc
+	return n
+end
+
+function GeneratedBumperLongueur(x)
+	math.randomseed(os.clock()*100000000000)
+	p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+		p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+		p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+		p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+	
+	n = 1+p+c+pp+cc+ppp+ccc+pppp+cccc
+	return n
+end
+
+function GeneratedBumper2(x)
+	math.randomseed(os.clock()*100000000000)
+	p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+	p = math.random(0, 1)
+	math.floor(p)
+	c = math.random(0, 1)
+	math.floor(c)
+	pp = math.random(0, 1)
+	math.floor(pp)
+	cc = math.random(0, 1)
+	math.floor(cc)
+	ppp = math.random(0, 1)
+	math.floor(ppp)
+	ccc = math.random(0, 1)
+	math.floor(ccc)
+	pppp = math.random(0, 1)
+	math.floor(pppp)
+	cccc = math.random(0, 1)
+	math.floor(cccc)
+	
+	n = 1+p+c+pp+cc+ppp+ccc+pppp+cccc
 	return n
 end
 
@@ -66,15 +255,23 @@ end
 
 function onGameReady()
 	Background = createSprite("assets/Background.png", WINDOW_WIDTH,WINDOW_HEIGHT,0,0)
-	local backgroundBody1 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(-1000), ResolutionHauteur(1045), {ResolutionLargeur(5000), ResolutionHauteur(35)}) -- en bas
-	local backgroundBody1 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(-1000), 0, {ResolutionLargeur(5000), ResolutionHauteur(35)})	-- en haut
-	local backgroundBody1 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(50), 0, {ResolutionLargeur(100), ResolutionHauteur(600)})	-- à gauche
-	local backgroundBody1 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(1870), 0, {ResolutionLargeur(100), ResolutionHauteur(600)})	-- à droite
-	local backgroundBody1 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(1920+707/4+1+55+50), 0, {ResolutionLargeur(1), ResolutionHauteur(1500)})  -- cage droite
-	local backgroundBody1 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(-300), 0, {ResolutionLargeur(100), ResolutionHauteur(1500)}) -- cage gauche
+	local backgroundBody11 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(-1000), ResolutionHauteur(1045), {ResolutionLargeur(5000), ResolutionHauteur(35)}) -- en bas
+	local backgroundBody12 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(-1000), 0, {ResolutionLargeur(5000), ResolutionHauteur(35)})	-- en haut
+	local backgroundBody13 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(50), 0, {ResolutionLargeur(100), ResolutionHauteur(600)})	-- à gauche
+	local backgroundBody14 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(1870), 0, {ResolutionLargeur(100), ResolutionHauteur(600)})	-- à droite
+	local backgroundBody15 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(1920+707/4+1+55+50), 0, {ResolutionLargeur(1), ResolutionHauteur(1500)})  -- cage droite
+	local backgroundBody16 = createBody("Static", "Box", 1, 0.1,0,false, ResolutionLargeur(-300), 0, {ResolutionLargeur(100), ResolutionHauteur(1500)}) -- cage gauche
+	local BackGroundSprite12 = createSprite("assets/BackGroundSprite.jpg", ResolutionLargeur(5000)*2,ResolutionHauteur(35)*2,ResolutionLargeur(-1000)*2,0*2)
+	attachSpriteTo(BackGroundSprite12, backgroundBody12, -0.5, -0.5)
+	local BackGroundSprite13 = createSprite("assets/BackGroundSprite.jpg", ResolutionLargeur(100)*2,ResolutionHauteur(600)*2,ResolutionLargeur(50)*2,0*2)
+	attachSpriteTo(BackGroundSprite13, backgroundBody13, -0.5, -0.5)
+	local BackGroundSprite14 = createSprite("assets/BackGroundSprite.jpg", ResolutionLargeur(100)*2,ResolutionHauteur(600)*2,ResolutionLargeur(1870)*2,0*2)
+	attachSpriteTo(BackGroundSprite14, backgroundBody14, -0.5, -0.5)
+	local BackGroundSprite11 = createSprite("assets/BackGroundSprite.jpg", ResolutionLargeur(5000)*2,ResolutionHauteur(35)*2,ResolutionLargeur(1000)*2,ResolutionHauteur(1045)*2)
+	attachSpriteTo(BackGroundSprite11, backgroundBody11, -0.5, -0.5)
 	--local groundPhysics = physicalizeWithOffset(Background, 1, 1, "Static", 0, 1)
-	knight = createAnimation("assets/Knight_anim/walk.png", "assets/Knight_anim/walk.a", ResolutionLargeur(587/4),ResolutionHauteur(707/4), ResolutionLargeur(250),ResolutionHauteur(700), 0.1) 
-	knight2 = createAnimation("assets/Knight_anim/walk.png", "assets/Knight_anim/walk.a", ResolutionLargeur(587/4),ResolutionHauteur(707/4), ResolutionLargeur(1500),ResolutionHauteur(700), 0.1) 
+	knight = createAnimation("assets/Knight_anim/walk.png", "assets/Knight_anim/walk.a", ResolutionLargeur(500/4),ResolutionHauteur(600/4), ResolutionLargeur(250),ResolutionHauteur(700), 0.1) 
+	knight2 = createAnimation("assets/Knight_anim/walk.png", "assets/Knight_anim/walk.a", ResolutionLargeur(500/4),ResolutionHauteur(600/4), ResolutionLargeur(1500),ResolutionHauteur(700), 0.1) 
 	knightBody = physicalizeWithOffset(knight, 1, 4, 0,false,"Dynamic", "Box",0.28, 0.09)
 	knight2Body = physicalizeWithOffset(knight2, 1, 4, 0,false,"Dynamic", "Box",0.28, 0.09)
 	local ballSprite1, BallBody1 = createBall1()
@@ -83,8 +280,37 @@ function onGameReady()
 	setAngularDamping(knightBody, 1)
 	setAngularDamping(knight2Body, 1)
 	a =createText(""..NbGoalRed,"assets/Fonts/CHILLER.TTF", ResolutionHauteur(200), 0, 0, 0, 0, 255)
-	b =createText(""..NbGoalBlue,"assets/Fonts/CHILLER.TTF", ResolutionHauteur(200), ResolutionLargeur(1830), 0, 255, 0, 0)
+	b =createText(""..NbGoalBlue,"assets/Fonts/CHILLER.TTF", ResolutionHauteur(200), ResolutionLargeur(1770), 0, 255, 0, 0)
 	setTimer(lol, 3000,-1)
+	local pp2 = ResolutionLargeur(200*GeneratedBumper2())
+	local oo2 = ResolutionHauteur(870)
+	local ii2 = ResolutionLargeur(30*GeneratedBumper2())
+	local uu2 = ResolutionHauteur(4*GeneratedBumper())
+	local pp3 = ResolutionLargeur(200*GeneratedBumper())
+	local oo3 = ResolutionHauteur(570)
+	local oo4 = ResolutionHauteur(270)
+	local ii3 = ResolutionLargeur(30*GeneratedBumperLargeur())
+	local uu3 = ResolutionHauteur(4*GeneratedBumper())
+	local backgroundBody2 = createBody("Static", "Box", 1, 0.1,0,false, pp2, oo2,{ii2,uu2})
+	local backgroundBody3 = createBody("Static", "Box", 1, 0.1,0,false, pp3, oo2, {ii3,uu3})
+	local backgroundBody4 = createBody("Static", "Box", 1, 0.1,0,false, pp2, oo3, {ii2, uu2})
+	local backgroundBody5 = createBody("Static", "Box", 1, 0.1,0,false, pp3, oo3, {ii3, uu3})
+	local backgroundBody6 = createBody("Static", "Box", 1, 0.1,0,false, pp2, oo4, {ii2, uu2})
+	local backgroundBody7 = createBody("Static", "Box", 1, 0.1,0,false, pp3, oo4, {ii3, uu3})
+	local BackGroundSprite2 = createSprite("assets/BackGroundSprite.jpg", ii2*2,uu2*2,pp2*2,oo2*2)
+	attachSpriteTo(BackGroundSprite2, backgroundBody2, -0.5, -0.5)
+	local BackGroundSprite3 = createSprite("assets/BackGroundSprite.jpg", ii3*2,uu3*2,pp3*2,oo3*2)
+	attachSpriteTo(BackGroundSprite3, backgroundBody3, -0.5, -0.5)
+	local BackGroundSprite3 = createSprite("assets/BackGroundSprite.jpg", ii3*2,uu3*2,pp3*2,oo3*2)
+	attachSpriteTo(BackGroundSprite3, backgroundBody3, -0.5, -0.5)
+	local BackGroundSprite4	= createSprite("assets/BackGroundSprite.jpg", ii2*2,uu2*2,pp2*2,oo3*2)
+	attachSpriteTo(BackGroundSprite4, backgroundBody4, -0.5, -0.5)
+	local BackGroundSprite5	= createSprite("assets/BackGroundSprite.jpg", ii3*2,uu3*2,pp3*2,oo3*2)
+	attachSpriteTo(BackGroundSprite5, backgroundBody5, -0.5, -0.5)
+	local BackGroundSprite6	= createSprite("assets/BackGroundSprite.jpg", ii2*2,uu2*2,pp2*2,oo4*2)
+	attachSpriteTo(BackGroundSprite6, backgroundBody6, -0.5, -0.5)
+	local BackGroundSprite7	= createSprite("assets/BackGroundSprite.jpg", ii3*2,uu3*2,pp3*2,oo4*2)
+	attachSpriteTo(BackGroundSprite7, backgroundBody7, -0.5, -0.5)
 end
 AddEventHandler("OnEngineLoad", onGameReady)
 
@@ -156,7 +382,17 @@ function onEngineRender()
 	victory = false
 	local d =createText("Victory !","assets/Fonts/CHILLER.TTF", ResolutionLargeur(255), ResolutionLargeur(700), ResolutionHauteur(400), 0, 0, 255)
 	end
-	
+	if AddBumper == 2 then
+	AddBumper = 0
+	math.randomseed(os.clock()*10000000000000)
+	 BUMPERX= math.random(ResolutionLargeur(200), ResolutionLargeur(1600))
+	math.floor(BUMPERX)
+	BUMPERY= math.random(ResolutionHauteur(200), ResolutionHauteur(750))
+	math.floor(BUMPERY)
+	local BUMPER = createBody("Static", "Circle", 1, 4, 50,false,BUMPERX, BUMPERY, {ResolutionLargeur(30), 0})
+	local BackGroundSpriteBUMPER = createSprite("assets/Bumper.png", 30*2,30*2,BUMPERX*2,BUMPERY*2)
+	attachSpriteTo(BackGroundSpriteBUMPER, BUMPER, -0.5, -0.5)
+	end
 	
 end
 AddEventHandler("OnEngineRender", onEngineRender)
@@ -168,10 +404,6 @@ function OnMouseClick(EventType, MouseButton, PosX, PosY)
 	
 end
 AddEventHandler("OnMouseClick", OnMouseClick)
-
-function blockJump()
-	isJumpBlocked = false
-end
 
 function OnKeyboardInput(key, state)
 if victory == true then
